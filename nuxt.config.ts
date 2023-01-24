fb5fef2d328da7ed05e71b784553242ea52169bf
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-01-24 11:55:47 -0300

    add icon to global headers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  app: {
+    head: {
+      meta: [],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "https://picsum.photos/200/300",
+        },
+      ],
+    },
+  },
 });
