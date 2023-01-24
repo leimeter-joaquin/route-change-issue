28cae9c243e21ff3d06404668f608560eed01fa9
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-01-24 11:14:06 -0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="cont">
+      <NuxtLink to="/">HOME</NuxtLink>
+      <NuxtLink to="/profile">PROFILE</NuxtLink>
+    </div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+.cont {
+  display: flex;
+  gap: 4rem;
+  background-color: wheat;
+  padding: 2rem 4rem;
+}
+
+.cont a {
+  background-color: rgb(52, 51, 53);
+  padding: 1rem 2rem;
+  color: azure;
+}
+</style>
